dd5c7d071fabf56861172c4e8d7b49c9a28dc585
Trevor <trevorfehrman@gmail.com>
2018-09-14 11:31:23 -0700

    Logout working

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import Login from "./containers/login";
 import Jokes from "./components/users";
 
 class App extends Component {
+	handleLogout = () => {
+		localStorage.removeItem("token");
+	};
 	render() {
 		return (
 			<div className="App">
 				<h1>Hi Earth</h1>
 				<Register />
 				<Login />
+				<button onClick={this.handleLogout}>Logout</button>
 				<Route path="/jokes" render={() => <Jokes />} />
 				<Link to="/jokes">
 					<button>Display jokes</button>
