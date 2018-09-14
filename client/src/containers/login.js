76ef8b305f5e8d435622e2105d580b5eed352a7d
Trevor <trevorfehrman@gmail.com>
2018-09-14 11:00:44 -0700

    Login implemented.  debugging

---
+++ client/src/containers/login.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Form from "../components/form";
+import axios from "axios";
+
+const API = "http://localhost:3300/api/login";
+
+class Login extends Component {
+	state = {
+		username: "",
+		password: "",
+		loading: false,
+		message: "",
+	};
+	handleChange = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	handleLogin = user => {
+		this.setState({ loading: true });
+		axios.post(API, user).then(response => {
+			localStorage.setItem("token", response.data.token);
+			this.setState({
+				loading: false,
+				message: "Successfully registered",
+			});
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<Form
+					name={"Login"}
+					handleChange={this.handleChange}
+					username={this.username}
+					password={this.password}
+				/>
+				<p>{this.state.message}</p>
+			</div>
+		);
+	}
+}
+
+export default Login;
